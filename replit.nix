7e10b85dea898f0dee8761faee00b83a02975f85
samote-lykn <samote.lykn@gmail.com>
2025-02-08 17:25:09 +0100

    feat(core): replit.nix

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{pkgs}: {
+  deps = [
+    pkgs.zlib
+    pkgs.xcodebuild
+    pkgs.geckodriver
+    pkgs.coreutils
+    pkgs.cacert
+    pkgs.glibcLocales
+    pkgs.libxcrypt
+    pkgs.pkg-config
+    pkgs.libffi
+  ];
+}
